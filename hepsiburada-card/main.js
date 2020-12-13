9ad81b9b89d595a5a0ec93bcd625a298c3ece4b3
emre-sozen <szn.emre1@gmail.com>
2020-12-13 20:25:39 +0300

    I add a files.

---
+++ hepsiburada-card/main.js
@@ -0,0 +1,8 @@
+function result() {
+    document.getElementById("product-text").style.visibility= "hidden";
+    document.getElementById("product-price").style.visibility= "visible";
+}
+function notresult() {
+    document.getElementById("product-text").style.visibility= "visible";
+    document.getElementById("product-price").style.visibility= "hidden";
+}
